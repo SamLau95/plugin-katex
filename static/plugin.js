bc29191a99b844eb791f8170aff9d21d76d744dd
Sam Lau <samlau95@gmail.com>
2018-01-14 00:47:01 -0800

    Allow align env

--- static/plugin.js
+++ static/plugin.js
@@ -3,6 +3,10 @@
 require(['gitbook'], function(gitbook) {
   var elCLass = '.page-inner'
   var katexOpts = {
+    macros: {
+      "\\begin{align*}": "\\begin{aligned}",
+      "\\end{align*}": "\\end{aligned}",
+    },
     delimiters: [
       { left: '$$', right: '$$', display: true },
       { left: '\\[', right: '\\]', display: true },
